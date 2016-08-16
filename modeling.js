bafb41389d0545adef096cc23268d6ecbb2403f4
Utkin Artem <solid.utkin@gmail.com>
2016-08-16 10:47:31 +0300

    add start configuration and motion loop

--- modeling.js
+++ modeling.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
   function initSquare() {
     var material = new THREE.LineBasicMaterial({
-  	color: 0x0000ff
+  	color: 0x000000
     });
 
     for(var i = 0; i <= 200; i = i + 10) {
@@ -38,22 +38,46 @@ window.onload = function() {
   initSquare();
 
   function createCoub(x,y) {
-    var cube_geometry = new THREE.BoxGeometry(10,10,10);
+    var cube_geometry = new THREE.BoxGeometry(10,10,0);
     var cube_texture = new THREE.MeshBasicMaterial({color: 0x0000ff});
     var cube = new THREE.Mesh(cube_geometry,cube_texture);
     cube.position.x = x + 5;
-    cube.position.y = x + 5;
-    cube.position.z = 5;
+    cube.position.y = y + 5;
+    cube.position.z = 0;
 
     scene.add(cube);
   }
-  createCoub(0, 0);
-  //createCoub(50, 70);
+
+  function removeCoubs() {
+    scene.children.forEach(function(object){
+      scene.remove(object);
+    });
+  }
+
+  var sideSize = 10;
+
+  function randomGen() {
+    for(var i = 0; i < 20 * sideSize; i = i + sideSize) {
+      for(var j = 0; j < 20 * sideSize; j = j + sideSize) {
+        if(Math.random() >= 0.5) {
+          createCoub(i, j);
+        }
+      }
+    }
+  }
+  randomGen();
 
   render = new THREE.WebGLRenderer({ alpha: true }); // рендер белого фона
   render.setSize(window.innerWidth,window.innerHeight);
   container.appendChild(render.domElement);
 
-  render.render(scene,camera);
+  animation();
 
+  function animation() {
+    requestAnimationFrame(animation);
+    //randomGen();
+    //initSquare();
+    //removeCoubs();
+    render.render(scene,camera);
+  }
 }
